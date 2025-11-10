658f05ab7501c1f56d5c4c40f582111ff33433d3
Hala Albrazi <hala.albarazii@gmail.com>
2025-11-10 14:10:41 +0200

    Add files via upload

---
+++ P9_Python.py
@@ -0,0 +1,14 @@
+print("Welcome to place the rabbit!")
+plants = [['🌿', '🌿', '🌿'], ['🌿', '🌿', '🌿'], ['🌿', '🌿', '🌿']]
+print(f"{plants[0]}\n{plants[1]}\n{plants[2]}\n")
+print("Whre should the rabbit go? 🐇")
+place = input("Please choose a row and a column! \n")
+row = int(place[0])
+column = int(place[1])
+plants[row-1][column-1] = "🐇"
+print("success!!!!! \n")
+print(f"{plants[0]}\n{plants[1]}\n{plants[2]}\n")
+
+
+# 🐇
+# 🌿
